8b17963d401a7aed7f0e632a5ca6f461ff03d169
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-09-01 16:11:56 +0900

    feat: フォーム送信されたURLのチェック処理部分を作成(#5)

--- src/js/qr.js
+++ src/js/qr.js
@@ -1,3 +1,22 @@
 import '../css/reset.css';
 import '../css/base.css';
 import '../css/qr.css';
+
+// アラート表示時に CSS が適用されてない状態を避けるために、DOMContentLoaded でなく load にしている
+window.addEventListener('load', () => {
+  const href = window.location.href;
+  const url = new URL(href);
+  const query = new URLSearchParams(url.search);
+
+  const inputUrl = query.get('url');
+
+  try {
+    const targetUrl = new URL(inputUrl);
+    console.log(targetUrl);
+  } catch (e) {
+    window.alert(
+      'Because a value not in URL format was entered, QR code generation is skipped and the user is returned to the top screen.',
+    );
+    window.location.href = '/';
+  }
+});
