9a7c03026d5e7f35f77f519993dddb1332b13e9a
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-09-01 20:33:44 +0900

    feat: QRコード画像ダウンロードの機能作成(#5)

--- src/js/qr.js
+++ src/js/qr.js
@@ -1,3 +1,5 @@
+/* globals HTMLAnchorElement */
+
 import '../css/reset.css';
 import '../css/base.css';
 import '../css/qr.css';
@@ -9,19 +11,44 @@ window.addEventListener('load', () => {
   const href = window.location.href;
   const url = new URL(href);
   const query = new URLSearchParams(url.search);
-
   const inputUrl = query.get('url');
+
+  const qrPreview = document.getElementById('qr-preview');
+  const qrDownloadLinkButton = document.getElementById(
+    'qr-download-link-button',
+  );
 
   try {
     const targetUrl = new URL(inputUrl);
     QRCode.toCanvas(
-      document.getElementById('qr-preview'),
+      qrPreview,
       targetUrl.href,
       { errorCorrectionLevel: 'M', width: 248, margin: 0 },
       (error) => {
         if (error) {
           console.error(error);
           window.alert('QR code generation failed.');
+        }
+      },
+    );
+    // QR コード画像データ URL をダウンロードリンクボタンの a タグにセット
+    QRCode.toDataURL(
+      qrPreview,
+      targetUrl.href,
+      {
+        errorCorrectionLevel: 'M',
+        type: 'image/webp',
+        width: 248,
+        margin: 0,
+      },
+      (error, url) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+
+        if (qrDownloadLinkButton instanceof HTMLAnchorElement) {
+          qrDownloadLinkButton.href = url;
         }
       },
     );
